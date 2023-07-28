a96bc12fb3d09b3652be2cbe73a87d31f576bef6
Sora-210 <sokominto1231@gmail.com>
2023-07-28 15:40:33 +0000

    add: タブでパネルを切り替えるコンポーネントの追加

---
+++ yumemi/components/STab.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tab-buttom" :class="props.isActive ? 'active' : ''">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+.tab-buttom {
+  display: inline-block;
+  padding: 0.3em 1.5em;
+  cursor: pointer;
+  transition: background-color ease 0.3s;
+  white-space: nowrap;
+}
+.tab-buttom:hover {
+  background-color: #c6dbf080;
+  transition: background-color ease 0.3s;
+}
+
+.active {
+  border-bottom: solid 5px #85c7de;
+  animation-name: active;
+  animation-duration: 0.3s;
+  animation-fill-mode: both;
+}
+
+@keyframes active {
+  0% {
+    border-bottom: solid 5px #ffffff;
+  }
+  100% {
+    border-bottom: solid 5px #85c7de;
+  }
+}
+</style>
---
+++ yumemi/components/STabList.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav class="tab-list">
+    <ul>
+      <li v-for="tab in props.tabs" :key="tab.key">
+        <s-tab :is-active="activeTab === tab.key" @click="changeValue(tab.key)">
+          {{ tab.name }}
+        </s-tab>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts">
+const activeTab = ref('')
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: false,
+    default: '',
+  },
+  tabs: {
+    type: Array<{
+      name: String
+      key: String
+    }>,
+    required: false,
+    default: [],
+  },
+})
+const emits = defineEmits<{ (e: 'update:modelValue', value: String): void }>()
+
+onMounted(() => {
+  activeTab.value = props.modelValue
+})
+
+const changeValue = (v) => {
+  activeTab.value = v
+  emits('update:modelValue', v)
+}
+
+watch(
+  () => props.modelValue,
+  (v) => {
+    activeTab.value = v
+  }
+)
+</script>
+
+<style scoped>
+.tab-list {
+  overflow-x: scroll;
+}
+.tab-list ul {
+  display: flex;
+  flex-direction: row;
+}
+</style>
