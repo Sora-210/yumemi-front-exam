037c8eb70ac13145c67cd8e784b9dddc92b38796
Sora-210 <sokominto1231@gmail.com>
2023-07-28 13:36:09 +0000

    add: カードコンポーネントの追加
    まとまりをわかりやすくする際の表示を共通化するため

---
+++ yumemi/components/SCardTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card-title">
+    <h6>
+      <slot></slot>
+    </h6>
+  </div>
+</template>
+
+<style scoped>
+.card-title {
+  font-size: 2em;
+  padding-left: 10px;
+  margin-bottom: 0.25em;
+  border-left: solid 5px #aed1e6;
+}
+</style>
